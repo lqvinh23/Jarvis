57d5a30c9337b07d537318cd396d37155f153c70
lqvinh23 <galacticos2cr7@gmail.com>
2022-11-08 21:11:47 +0700

    First commit

---
+++ sketch_1/sketch_1.ino
@@ -0,0 +1,308 @@
+#include <Keypad.h>
+#include <EEPROM.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <ArduinoJson.h>
+#include "DHT.h"
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+Servo sg90;
+StaticJsonDocument<256> doc;
+
+#define servoPin 13                //Controls the Servo
+#define doorBtn 22                //Push Button
+#define li_light 24
+#define li_lightBtn 26
+
+bool s_li_light = false;
+bool s_door = false;
+
+float humidity = 0;
+float temperature = 0;
+
+float lastMillis = 0;
+
+const int DHTPIN = 28;
+const int DHTTYPE = DHT11;
+DHT dht(DHTPIN, DHTTYPE);
+
+/* ------------------------------KEYPAD--------------------------------*/
+const byte numRows = 4;         //number of rows on the keypad
+const byte numCols = 4;         //number of columns on the keypad
+
+char keymap[numRows][numCols] =
+{
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+char keypressed;                 //Where the keys are stored it changes very often
+char code[] = {'6', '6', '0', '1'}; //The default code, you can change it or make it a 'n' digits one
+
+char code_buff1[sizeof(code)];  //Where the new key is stored
+char code_buff2[sizeof(code)];  //Where the new key is stored again so it's compared to the previous one
+
+short a = 0, i = 0, s = 0, j = 0; //Variables used for keypad's purpose
+
+byte rowPins[numRows] = {23, 25, 27, 29}; //Rows 0 to 3 //if you modify your pins you should modify this too
+byte colPins[numCols] = {31, 33, 35, 37}; //Columns 0 to 3
+
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+void setup()
+{
+  doc["frontDoor"] = 0;
+  doc["livingroomLight"] = 0;
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  dht.begin();
+  lcd.init();
+  lcd.begin(16, 2);
+  lcd.backlight();
+  //  lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
+  //  lcd.setBacklight(HIGH); //Lighting backlight
+  //  lcd.home ();
+  lcd.print("Final project");      //What's written on the LCD you can change
+  sg90.attach(servoPin);
+  sg90.write(0);
+  //  pinMode(Servo, OUTPUT);
+  pinMode(doorBtn, INPUT_PULLUP);
+  pinMode(li_light, OUTPUT);
+  pinMode(li_lightBtn, INPUT_PULLUP);
+
+  //  for(i=0 ; i<sizeof(code);i++){        //When you upload the code the first time keep it commented
+  //    EEPROM.get(i, code[i]);             //Upload the code and change it to store it in the EEPROM
+  //  }                                  //Then uncomment this for loop and reupload the code (It's done only once)
+
+}
+
+
+void loop()
+{
+  GetDataFromESP();
+  ReadNumpad();
+  ReadButton();
+  ReadSensor();
+}
+
+void ReadSensor() {
+  if ((unsigned long) (millis() - lastMillis) > 5000) {
+    humidity = dht.readHumidity();
+    Serial.print("Humid: "); Serial.println(humidity);
+    temperature = dht.readTemperature();
+    doc["humidity"] = humidity;
+    doc["temperature"] = temperature;
+    serializeJson(doc, Serial1);
+    lastMillis = millis();
+  }
+}
+
+void ReadButton() {
+  if (digitalRead(doorBtn) == LOW) {  //Opening by the push button
+    delay(30);
+    s_door = !s_door;
+    if (s_door) {
+      sg90.write(90);
+      doc["frontDoor"] = 1;
+      serializeJson(doc, Serial1);
+    }
+    else {
+      sg90.write(0);
+      doc["frontDoor"] = 0;
+      serializeJson(doc, Serial1);
+    }
+    while (digitalRead(doorBtn) == LOW) {}
+  }
+
+  if (digitalRead(li_lightBtn) == LOW) {  //Turning on/off by the push button
+    delay(30);
+    s_li_light = !s_li_light;
+    digitalWrite(li_light, s_li_light);
+    doc["livingroomLight"] = (int)s_li_light;
+    serializeJson(doc, Serial1);
+    while (digitalRead(li_lightBtn) == LOW) {}
+  }
+}
+
+void ReadNumpad() {
+  keypressed = myKeypad.getKey();               //Constantly waiting for a key to be pressed
+  if (keypressed == '*') {                    // * to open the lock
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Enter code");            //Message to show
+    GetCode();                          //Getting code function
+    if (a == sizeof(code))        //The GetCode function assign a value to a (it's correct when it has the size of the code array)
+      OpenDoor();                   //Open lock function if code is correct
+    else {
+      lcd.clear();
+      lcd.print("Wrong");          //Message to print when the code is wrong
+    }
+    delay(2000);
+    lcd.clear();
+    lcd.print("Final project");             //Return to standby mode it's the message do display when waiting
+  }
+
+  if (keypressed == '#') {                //To change the code it calls the changecode function
+    ChangeCode();
+    lcd.clear();
+    lcd.print("Final project");                 //When done it returns to standby mode
+  }
+
+  if (keypressed == 'C') {
+    s_door = false;
+    sg90.write(0);
+    doc["frontDoor"] = 0;
+    serializeJson(doc, Serial1);
+  }
+}
+
+void GetDataFromESP() {
+  if (Serial1.available()) {
+    DeserializationError err = deserializeJson(doc, Serial1);
+    if (err) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(err.f_str());
+      return;
+    }
+    bool s_li_light = doc["livingroomLight"];
+    digitalWrite(li_light, s_li_light);
+    bool s_door = doc["frontDoor"];
+    if (s_door) {
+      sg90.write(90);
+    }
+    else {
+      sg90.write(0);
+    }
+  }
+}
+
+void GetCode() {                 //Getting code sequence
+  i = 0;                         //All variables set to 0
+  a = 0;
+  j = 0;
+
+  while (keypressed != 'A') {                             //The user press A to confirm the code otherwise he can keep typing
+    keypressed = myKeypad.getKey();
+    if (keypressed != NO_KEY && keypressed != 'A' ) {     //If the char typed isn't A and neither "nothing"
+      lcd.setCursor(j, 1);                                //This to write "*" on the LCD whenever a key is pressed it's position is controlled by j
+      lcd.print("*");
+      j++;
+      if (keypressed == code[i] && i < sizeof(code)) {    //if the char typed is correct a and i increments to verify the next caracter
+        a++;                                              //Now I think maybe I should have use only a or i ... too lazy to test it -_-'
+        i++;
+      }
+      else
+        a--;                                              //if the character typed is wrong a decrements and cannot equal the size of code []
+    }
+  }
+  keypressed = NO_KEY;
+
+}
+
+void ChangeCode() {                     //Change code sequence
+  lcd.clear();
+  lcd.print("Changing code");
+  delay(1000);
+  lcd.clear();
+  lcd.print("Enter old code");
+  GetCode();                      //verify the old code first so you can change it
+
+  if (a == sizeof(code)) {  //again verifying the a value
+    lcd.clear();
+    lcd.print("Changing code");
+    GetNewCode1();            //Get the new code
+    GetNewCode2();            //Get the new code again to confirm it
+    s = 0;
+    for (i = 0 ; i < sizeof(code) ; i++) { //Compare codes in array 1 and array 2 from two previous functions
+      if (code_buff1[i] == code_buff2[i])
+        s++;                                //again this how we verifiy, increment s whenever codes are matching
+    }
+    if (s == sizeof(code)) {        //Correct is always the size of the array
+
+      for (i = 0 ; i < sizeof(code) ; i++) {
+        code[i] = code_buff2[i];       //the code array now receives the new code
+        EEPROM.put(i, code[i]);        //And stores it in the EEPROM
+
+      }
+      lcd.clear();
+      lcd.print("Code Changed");
+      delay(2000);
+    }
+    else {                        //In case the new codes aren't matching
+      lcd.clear();
+      lcd.print("Codes are not");
+      lcd.setCursor(0, 1);
+      lcd.print("matching !!");
+      delay(2000);
+    }
+
+  }
+
+  else {                    //In case the old code is wrong you can't change it
+    lcd.clear();
+    lcd.print("Wrong");
+    delay(2000);
+  }
+}
+
+void GetNewCode1() {
+  i = 0;
+  j = 0;
+  lcd.clear();
+  lcd.print("Enter new code");   //tell the user to enter the new code and press A
+  lcd.setCursor(0, 1);
+  lcd.print("and press A");
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("and press A");     //Press A keep showing while the top row print ***
+
+  while (keypressed != 'A') {          //A to confirm and quits the loop
+    keypressed = myKeypad.getKey();
+    if (keypressed != NO_KEY && keypressed != 'A' ) {
+      lcd.setCursor(j, 0);
+      lcd.print("*");               //On the new code you can show * as I did or change it to keypressed to show the keys
+      code_buff1[i] = keypressed;   //Store caracters in the array
+      i++;
+      j++;
+    }
+  }
+  keypressed = NO_KEY;
+}
+
+void GetNewCode2() {                        //This is exactly like the GetNewCode1 function but this time the code is stored in another array
+  i = 0;
+  j = 0;
+
+  lcd.clear();
+  lcd.print("Confirm code");
+  lcd.setCursor(0, 1);
+  lcd.print("and press A");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("and press A");
+
+  while (keypressed != 'A') {
+    keypressed = myKeypad.getKey();
+    if (keypressed != NO_KEY && keypressed != 'A' ) {
+      lcd.setCursor(j, 0);
+      lcd.print("*");
+      code_buff2[i] = keypressed;
+      i++;
+      j++;
+    }
+  }
+  keypressed = NO_KEY;
+}
+
+void OpenDoor() {            //Lock opening function open for 3s
+  lcd.clear();
+  lcd.print("Welcome");       //With a message printed
+  sg90.write(90);
+  delay(3000);
+  sg90.write(0);
+}
---
+++ sketch_1_esp/sketch_1_esp.ino
@@ -0,0 +1,199 @@
+#include <PubSubClient.h>
+#include "ThingsBoard.h"
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+#define WIFI_AP "Spiderman"
+#define WIFI_PASSWORD "vinh2223"
+
+char Thingsboard_Server[] = "demo.thingsboard.io";
+#define TOKEN "MispEwH7Jt4A4EbbSAhe"
+
+
+WiFiClient wifiClient;
+WiFiClient espClient;
+
+ThingsBoard tb(espClient);
+PubSubClient client(wifiClient);
+
+SoftwareSerial mega(D2, D3); //rx,tx
+int status = WL_IDLE_STATUS;
+
+StaticJsonDocument<256> doc;
+
+char ch;
+float lastSend = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  mega.begin(9600);
+  InitWiFi();
+  client.setServer(Thingsboard_Server, 1883);
+  client.setCallback(callback_sub);
+
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  if (mega.available() > 0)
+  {
+    DeserializationError err = deserializeJson(doc, mega);
+    // Test if parsing succeeds.
+    if (err == DeserializationError::Ok) {
+      bool frontDoor = doc["frontDoor"];
+      bool livingroomLight = doc["livingroomLight"];
+      float humidity = doc["humidity"];
+      float temperature = doc["temperature"];
+      // Print values.
+      Serial.print("frontDoor: ");
+      Serial.println(frontDoor);
+      Serial.print("livingroomLight: ");
+      Serial.println(livingroomLight);
+      Serial.print("humidity: ");
+      Serial.println(humidity);
+      SendDataToThingsboard(livingroomLight, frontDoor, humidity, temperature);
+    }
+    else
+    {
+      // Print error to the "debug" serial port
+      Serial.print("deserializeJson() returned ");
+      Serial.println(err.c_str());
+
+      // Flush all bytes in the "link" serial port buffer
+      while (mega.available() > 0)
+        mega.read();
+    }
+  }
+
+  client.loop();
+}
+
+void callback_sub(const char* topic, byte* payload, unsigned int length)
+{
+  Serial.println("Yes");
+  StaticJsonDocument<256> data;
+  deserializeJson(data, payload, length);
+  String method1 = data["method"].as<String>();
+  if (method1 == "livingroomLight")
+  {
+    if (data["params"] == true)
+    {
+      doc["livingroomLight"] = 1;
+      serializeJson(doc, mega);
+      String payload01 = "{\"livingroomLight\":\"1\"}";
+      char attributes01[100];
+      payload01.toCharArray( attributes01, 100 );
+      client.publish( "v1/devices/me/attributes", attributes01 );
+    }
+    else if (data["params"] == false)
+    {
+      doc["livingroomLight"] = 0;
+      serializeJson(doc, mega);
+      String payload01 = "{\"livingroomLight\":\"0\"}";
+      char attributes01[100];
+      payload01.toCharArray( attributes01, 100 );
+      client.publish( "v1/devices/me/attributes", attributes01 );
+    }
+  }
+
+  if (method1 == "frontDoor")
+  {
+    if (data["params"] == true)
+    {
+      doc["frontDoor"] = 1;
+      serializeJson(doc, mega);
+      String payload02 = "{\"frontDoor\":\"1\"}";
+      char attributes02[100];
+      payload02.toCharArray( attributes02, 100 );
+      client.publish( "v1/devices/me/attributes", attributes02 );
+    }
+    else if (data["params"] == false)
+    {
+      doc["frontDoor"] = 0;
+      serializeJson(doc, mega);
+      String payload02 = "{\"frontDoor\":\"0\"}";
+      char attributes02[100];
+      payload02.toCharArray( attributes02, 100 );
+      client.publish( "v1/devices/me/attributes", attributes02 );
+    }
+  }
+}
+
+void SendDataToThingsboard(int livingroomLight, int frontDoor, float humidity, float temperature)
+{
+  if ( millis() - lastSend > 1000 )
+  {
+    // Prepare a JSON payload string
+    String payload = "{";
+    payload += "\"livingroomLight\":\"" + (String)livingroomLight + "\"}";
+    char attributes[100];
+    payload.toCharArray( attributes, 100 );
+    client.publish( "v1/devices/me/attributes", attributes );
+
+    String payload1 = "{";
+    payload1 += "\"frontDoor\":\"" + (String)frontDoor + "\"}";
+    char attributes1[100];
+    payload1.toCharArray( attributes1, 100 );
+    client.publish( "v1/devices/me/attributes", attributes1 );
+
+    String payload2 = "{";
+    payload2 += "\"humidity\":\"" + (String)humidity + "\"}";
+    char attributes2[100];
+    payload2.toCharArray( attributes2, 100 );
+    client.publish( "v1/devices/me/telemetry", attributes2 );
+
+    String payload3 = "{";
+    payload3 += "\"temperature\":\"" + (String)temperature + "\"}";
+    char attributes3[100];
+    payload3.toCharArray( attributes3, 100 );
+    client.publish( "v1/devices/me/telemetry", attributes3 );
+
+    Serial.println("Sent data to Thingsboard ");
+  }
+  lastSend = millis();
+}
+
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Connected to AP");
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to AP");
+  }
+  Serial.print("Connecting to Thingsboard node ...");
+  if ( client.connect("ESP8266 Device", TOKEN, NULL) ) {
+    Serial.println( "[DONE]" );
+    client.subscribe("v1/devices/me/rpc/request/+");
+  } else {
+    Serial.print( "[FAILED]" );
+    Serial.println( " : retrying in 5 seconds]" );
+    // Wait 5 seconds before retrying
+    delay( 5000 );
+  }
+}
