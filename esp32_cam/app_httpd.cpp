4e3388552f4b3601b9784e7250de3047014a6dcf
lqvinh23 <galacticos2cr7@gmail.com>
2022-11-24 20:34:54 +0700

    ??

--- esp32_cam/app_httpd.cpp
+++ esp32_cam/app_httpd.cpp
@@ -652,16 +652,16 @@ void startCameraServer(){
     
     face_id_init(&id_list, FACE_ID_SAVE_NUMBER, ENROLL_CONFIRM_TIMES);
     
-    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
-    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(camera_httpd, &index_uri);
-        httpd_register_uri_handler(camera_httpd, &cmd_uri);
-        httpd_register_uri_handler(camera_httpd, &status_uri);
-        httpd_register_uri_handler(camera_httpd, &capture_uri);
-    }
+//    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+//    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+//        httpd_register_uri_handler(camera_httpd, &index_uri);
+//        httpd_register_uri_handler(camera_httpd, &cmd_uri);
+//        httpd_register_uri_handler(camera_httpd, &status_uri);
+//        httpd_register_uri_handler(camera_httpd, &capture_uri);
+//    }
 
-    config.server_port += 1;
-    config.ctrl_port += 1;
+    config.server_port = 80;
+//    config.ctrl_port += 1;
     Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
     if (httpd_start(&stream_httpd, &config) == ESP_OK) {
         httpd_register_uri_handler(stream_httpd, &stream_uri);
--- esp32_cam/esp32_cam.ino
+++ esp32_cam/esp32_cam.ino
@@ -2,10 +2,6 @@
 //ESP Camera Artificial Intelligence Face Detection Automatic Door Lock
 #include "esp_camera.h"
 #include <WiFi.h>
-#include <PubSubClient.h>
-#include "ThingsBoard.h"
-#include <SoftwareSerial.h>
-#include <ArduinoJson.h>
 
 // Select camera model
 //#define CAMERA_MODEL_WROVER_KIT
@@ -26,23 +22,6 @@ bool matchFace = false;
 bool activateRelay = false;
 long prevMillis=0;
 int interval = 5000;
-
-/* ---------------------Thingsboard-------------------- */
-char Thingsboard_Server[] = "demo.thingsboard.io";
-#define TOKEN "MispEwH7Jt4A4EbbSAhe"
-
-
-WiFiClient wifiClient;
-WiFiClient espClient;
-
-ThingsBoardSized<256, 32> tb(espClient);
-PubSubClient client(wifiClient);
-
-// SoftwareSerial mega(D2, D3); //rx,tx
-int status = WL_IDLE_STATUS;
-
-StaticJsonDocument<1024> doc;
-/* ---------------------------------------------------- */
 
 void setup() {
   pinMode(Red,OUTPUT);
@@ -127,119 +106,23 @@ void setup() {
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
-
-/* ---------------------Thingsboard-------------------- */
-  client.setServer(Thingsboard_Server, 1883);
-  client.setCallback(callback_sub);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-
   FaceRecognize();
-
-  client.loop();
-  tb.loop();
 }
 
 void FaceRecognize() {
   if (matchFace==true && activateRelay==false) {
     activateRelay=true;
-    doc["frontDoor"] = 1;
-    serializeJsonPretty(doc, Serial);
-    digitalWrite(Green,HIGH);
+    digitalWrite(4,HIGH);
     digitalWrite(Red,LOW);
     prevMillis=millis();
   }
   if (activateRelay == true && millis()-prevMillis > interval) {
     activateRelay=false;
     matchFace=false;
-    doc["frontDoor"] = 0;
-    serializeJsonPretty(doc, Serial);
-    digitalWrite(Green,LOW);
+    digitalWrite(4,LOW);
     digitalWrite(Red,HIGH);
-  }
-}
-
-void callback_sub(const char* topic, byte* payload, unsigned int length)
-{
-  // StaticJsonDocument<256> data;
-  // deserializeJson(data, payload, length);
-
-  // String method1 = data["method"].as<String>();
-  // doc[method1] = (int)data["params"];
-
-  // String payload01 = "{" + method1 + ":" + doc[method1].as<String>() + "}";
-  // char attributes01[100];
-  // payload01.toCharArray( attributes01, 100 );
-  // client.publish( "v1/devices/me/attributes", attributes01 );
-}
-
-void SendDataToThingsboard()
-{
-  if ( millis() - lastSend > 1000 )
-  {
-    // float telemetry_val[2] = {
-    //   doc["temperature"].as<float>(),
-    //   doc["humidity"].as<float>(),
-    // };
-
-    // for (int i = 0; i < 2; i++) {
-    //   String payload = "{" + telemetries[i] + ":" + (String)telemetry_val[i] + "}";
-    //   char attributes[100];
-    //   payload.toCharArray( attributes, 100 );
-    //   client.publish( "v1/devices/me/telemetry", attributes );
-    // }
-
-    // int attribute_val[numberOfDevice] = {
-    //   doc["frontDoor"].as<int>(), 
-    //   doc["livingroomLight"].as<int>(), 
-    //   doc["livingroomFan"].as<int>(),
-    //   doc["kitchenLight"].as<int>(),
-    //   doc["kitchenFan"].as<int>(),
-    //   doc["bedroomLight"].as<int>(),
-    //   doc["bathroomLight"].as<int>(),
-    //   doc["theftMode"].as<int>(),
-    //   doc["theftDetect"].as<int>(),
-    //   doc["speaker"].as<int>(),
-    //   doc["gasLeak"].as<int>(),
-    //   doc["fire"].as<int>(),
-    //   doc["hanger"].as<int>(),
-    // };
-
-    // for (int i = 0; i < numberOfDevice; i++) {
-    //   String payload1 = "{" + devices[i] + ":" + (String)attribute_val[i] + "}";
-    //   char attributes1[100];
-    //   payload1.toCharArray( attributes1, 100 );
-    //   client.publish( "v1/devices/me/attributes", attributes1 );
-    // }
-
-    Serial.println("\nSent data to Thingsboard ");
-    lastSend = millis();
-  }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  status = WiFi.status();
-  if ( status != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("Connected to AP");
-  }
-  Serial.print("Connecting to Thingsboard node ...");
-  if ( client.connect("ESP8266 Device", TOKEN, NULL) ) {
-    Serial.println( "[DONE]" );
-    client.subscribe("v1/devices/me/rpc/request/+");
-  } else {
-    Serial.print( "[FAILED]" );
-    Serial.println( " : retrying in 5 seconds]" );
-    // Wait 5 seconds before retrying
-    delay( 5000 );
   }
 }
